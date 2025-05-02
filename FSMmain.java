0baea61f31782e7f18ece5add4504a8cd83c8411
tuanac <153655058+tuanacosgun@users.noreply.github.com>
2025-05-02 11:31:25 +0300

    Revert "Merge branch 'main' of https://github.com/edatan9/SE116-PROJECT"
    This reverts commit 268505d58d3b00cc0e1afe8e12d8e7cf7d7ea152, reversing
    changes made to 127da0b598f22e5e7df295c7b751a72d28e3cb91.

--- FSMmain.java
+++ FSMmain.java
@@ -1027,12 +1027,10 @@ class GitVersion {
             GIT_PROPERTIES.load(GitVersion.class.getClassLoader().getResourceAsStream("git.properties"));
         } catch (IOException | NullPointerException e) {
 
-
     static {
         try {
             GIT_PROPERTIES.load(GitVersion.class.getClassLoader().getResourceAsStream("git.properties"));
         } catch (IOException | NullPointerException e) {
-
         }
     }
 
